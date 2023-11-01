23e090726230325c6f9a3e85f95c0a5aa7c8d37c
Dmitry <mitya.shabat@gmail.com>
2023-11-01 20:32:10 +0400

    Add metadata for landmarks and inter-connections

--- mediapipe/examples/desktop/run_graph_gpu.h
+++ mediapipe/examples/desktop/run_graph_gpu.h
@@ -21,6 +21,56 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
+enum HandLandmark {
+    WRIST = 0,
+    THUMB_CMC = 1,
+    THUMB_MCP = 2,
+    THUMB_IP = 3,
+    THUMB_TIP = 4,
+    INDEX_FINGER_MCP = 5,
+    INDEX_FINGER_PIP = 6,
+    INDEX_FINGER_DIP = 7,
+    INDEX_FINGER_TIP = 8,
+    MIDDLE_FINGER_MCP = 9,
+    MIDDLE_FINGER_PIP = 10,
+    MIDDLE_FINGER_DIP = 11,
+    MIDDLE_FINGER_TIP = 12,
+    RING_FINGER_MCP = 13,
+    RING_FINGER_PIP = 14,
+    RING_FINGER_DIP = 15,
+    RING_FINGER_TIP = 16,
+    PINKY_MCP = 17,
+    PINKY_PIP = 18,
+    PINKY_DIP = 19,
+    PINKY_TIP = 20
+};
+
+typedef std::pair<HandLandmark, HandLandmark> LandmarkPair;
+
+const std::vector<LandmarkPair> landmarkConnections = {
+    {HandLandmark::WRIST, HandLandmark::THUMB_CMC},
+    {HandLandmark::THUMB_CMC, HandLandmark::THUMB_MCP},
+    {HandLandmark::THUMB_MCP, HandLandmark::THUMB_IP},
+    {HandLandmark::THUMB_IP, HandLandmark::THUMB_TIP},
+    {HandLandmark::WRIST, HandLandmark::INDEX_FINGER_MCP},
+    {HandLandmark::INDEX_FINGER_MCP, HandLandmark::INDEX_FINGER_PIP},
+    {HandLandmark::INDEX_FINGER_PIP, HandLandmark::INDEX_FINGER_DIP},
+    {HandLandmark::INDEX_FINGER_DIP, HandLandmark::INDEX_FINGER_TIP},
+    {HandLandmark::INDEX_FINGER_MCP, HandLandmark::MIDDLE_FINGER_MCP},
+    {HandLandmark::MIDDLE_FINGER_MCP, HandLandmark::MIDDLE_FINGER_PIP},
+    {HandLandmark::MIDDLE_FINGER_PIP, HandLandmark::MIDDLE_FINGER_DIP},
+    {HandLandmark::MIDDLE_FINGER_DIP, HandLandmark::MIDDLE_FINGER_TIP},
+    {HandLandmark::MIDDLE_FINGER_MCP, HandLandmark::RING_FINGER_MCP},
+    {HandLandmark::RING_FINGER_MCP, HandLandmark::RING_FINGER_PIP},
+    {HandLandmark::RING_FINGER_PIP, HandLandmark::RING_FINGER_DIP},
+    {HandLandmark::RING_FINGER_DIP, HandLandmark::RING_FINGER_TIP},
+    {HandLandmark::RING_FINGER_MCP, HandLandmark::PINKY_MCP},
+    {HandLandmark::WRIST, HandLandmark::PINKY_MCP},
+    {HandLandmark::PINKY_MCP, HandLandmark::PINKY_PIP},
+    {HandLandmark::PINKY_PIP, HandLandmark::PINKY_DIP},
+    {HandLandmark::PINKY_DIP, HandLandmark::PINKY_TIP}
+};
+
 struct LandmarkList {
     std::vector<cv::Point3f> landmarks;
     std::vector<float> presence;
